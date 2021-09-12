f03b61eafbe7e80a596c7e1eaf5ca2c17534d5a5
z9071855 <37895470+z9071855@users.noreply.github.com>
2021-09-12 07:54:05 -0400

    please work

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000
 
 server.get('/', function (req, res, next) {
     console.log('accessing the index page');
-    res.sendFile(path.join(__dirname, './letter-hunt.html'));
+    res.send('./letter-hunt.html');
 
 });
 
