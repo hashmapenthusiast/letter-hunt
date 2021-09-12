943700f4f1b1c6e937f98259d83658acba537f88
z9071855 <37895470+z9071855@users.noreply.github.com>
2021-09-12 07:58:41 -0400

    poupdate

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const server = express();
-const port = 3000
+const port = process.env.PORT || 5000
 
 server.get('/', function (req, res, next) {
     console.log('accessing the index page');
