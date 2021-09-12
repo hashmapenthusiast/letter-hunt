3d9bfc667273cc83205327e2540a909cbdc6d8f8
z9071855 <37895470+z9071855@users.noreply.github.com>
2021-09-12 07:34:14 -0400

    first-commit
    load into heroku next

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const server = express();
+const port = 3000
+
+server.use('/public', express.static(path.join(__dirname,'letter-hunt/002')));
+
+server.get('/', function (req, res, next) {
+    console.log('accessing the index page');
+    res.sendFile(path.join(__dirname,'letter-hunt/002', 'letter-hunt.html'));
+
+});
+
+server.listen(port, function () {
+    console.log(`listening on port ${port}`)
+});
