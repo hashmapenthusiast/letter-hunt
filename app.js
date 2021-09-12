a3e6a1b83ed6234f9d8239f66879a8f6172040cb
z9071855 <37895470+z9071855@users.noreply.github.com>
2021-09-12 08:01:43 -0400

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000
 
 server.get('/', function (req, res, next) {
     console.log('accessing the index page');
-    res.send('./letter-hunt.html');
+    res.sendFile('./letter-hunt.html');
 
 });
 
