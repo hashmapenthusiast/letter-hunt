3d8a40e770874de56e162d7592f492e60a7b5394
z9071855 <37895470+z9071855@users.noreply.github.com>
2021-09-13 07:14:27 -0400

    Update letter-hunt.js

--- letter-hunt/002/letter-hunt.js
+++ letter-hunt/002/letter-hunt.js
@@ -286,7 +286,7 @@ window.onload = function (event) {
 
     });
 
-    const DOMTELLER = function (e, carrier = null) {
+    const DOMTELLER = function (e, carrier) {
         console.log(e, e.key)
         let p = document.createElement('p')
         p.innerHTML = `eventype->${e.type} keypressed->${e.key} code->${e.code} data->${e.data} droidCarrier->${carrier}`;
