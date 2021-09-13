2280ccd7c4ee6dae3a6e6eb8af0e089c10e4dd4b
z9071855 <37895470+z9071855@users.noreply.github.com>
2021-09-13 00:35:10 -0400

    retrying

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const path = require('path');
 const server = express();
 const port = process.env.PORT || 5000
 
+server.use('/public', express.static(path.join(__dirname,'letter-hunt/002')));
+
 server.get('/', function (req, res, next) {
     console.log('accessing the index page');
-    res.sendFile(path.join(__dirname, 'letter-hunt.html'));
+    res.sendFile(path.join(__dirname,'letter-hunt/002', 'letter-hunt.html'));
 
 });
 
