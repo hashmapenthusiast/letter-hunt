b3d7cec1ce31362177b5fb68daf4a8e545a12437
z9071855 <37895470+z9071855@users.noreply.github.com>
2021-09-15 05:22:45 -0400

    remove string

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const path = require('path');
 const server = express();
 const port = process.env.PORT || 5000
-const version = 'letter-hunt/003'
 
 
-server.use('/public', express.static(path.join(__dirname, version)));
+server.use('/public', express.static(path.join(__dirname, 'letter-hunt/003')));
 
 server.get('/', function (req, res, next) {
     console.log('accessing the index page');
-    res.sendFile(path.join(__dirname, version, 'letter-hunt.html'));
+    res.sendFile(path.join(__dirname, 'letter-hunt/003', 'letter-hunt.html'));
 
 });
 
