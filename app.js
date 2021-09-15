8168d4a6cae76c9cf7f6c84388e4dbacb934cdc3
z9071855 <37895470+z9071855@users.noreply.github.com>
2021-09-15 02:59:09 -0400

    new directory

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const server = express();
 const port = process.env.PORT || 5000
+const version = 'letter-hunt/003'
 
-server.use('/public', express.static(path.join(__dirname,'letter-hunt/002')));
+
+server.use('/public', express.static(path.join(__dirname, version)));
 
 server.get('/', function (req, res, next) {
     console.log('accessing the index page');
-    res.sendFile(path.join(__dirname,'letter-hunt/002', 'letter-hunt.html'));
+    res.sendFile(path.join(__dirname, version, 'letter-hunt.html'));
 
 });
 
